e4faaad4b655f69bd22188bf45bb142418f17500
Matías Estrada <matiasestrada.i@gmail.com>
2013-09-02 19:12:08 -0400

    Added store/load params file for scale process

--- classify/helpers/scale_set.py
+++ classify/helpers/scale_set.py
@@ -13,9 +13,11 @@ LIBSVM_RELATIVE = 'classify/libs/libsvm/'
 
 if 'ASTROFY_HOME' in os.environ:
     TRAINING_SET_PATH = os.environ['ASTROFY_HOME'] + 'set/training.set'
-    SCALED_TRAINING_SET_PATH = os.environ['ASTROFY_HOME'] + 'set/training_scale.set'
+    SCALE_PATH = os.environ['ASTROFY_HOME'] + 'set/'
+    SCALED_TRAINING_SET_PATH = SCALE_PATH + 'training_scale.set'
     LIBSVM_PATH = os.environ['ASTROFY_HOME'] + LIBSVM_RELATIVE
 else:
+    SCALE_PATH = 'set/'
     TRAINING_SET_PATH = 'set/training.set'
     SCALED_TRAINING_SET_PATH = 'set/training_scale.set'
     LIBSVM_PATH = LIBSVM_RELATIVE
@@ -24,26 +26,31 @@ else:
 def scale(
     input_path=TRAINING_SET_PATH,
     output_path=SCALED_TRAINING_SET_PATH,
-    svm_scale='svm-scale'):
+    scale_params_path=SCALE_PATH + 'scale.params',
+    svm_scale='svm-scale',
+    scale_mode='r'):
 
-    COMMAND = "{svm_scale_path} {input_path} > {output_path}"
+    COMMAND = "{svm_scale_path} -{scale_mode} {scale_params_path} {input_path} > {output_path}"
 
-    logger.info("Scaling the training set")
-    logger.info("Input: {}".format(input_path))
-    logger.info("Output: {}".format(output_path))
+    logger.debug("Scaling the training set")
+    logger.debug("Input: {}".format(input_path))
+    logger.debug("Output: {}".format(output_path))
+    logger.debug("Storing scale params in: {}".format(scale_params_path))
 
     command = COMMAND.format(
         svm_scale_path=LIBSVM_PATH + svm_scale,
         input_path=input_path,
-        output_path=output_path)
+        output_path=output_path,
+        scale_params_path=scale_params_path,
+        scale_mode=scale_mode)
 
-    logger.info("Executing command:  " + command)
+    logger.debug("Executing command:  " + command)
 
     os.system(command)
 
-    logger.info("Scaling process finished.")
+    logger.debug("Scaling process finished.")
 
 
 if __name__ == '__main__':
 
-    scale()
+    scale(scale_mode='s')
