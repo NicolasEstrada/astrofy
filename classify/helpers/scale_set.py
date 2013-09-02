25f8479219ad3b2ecd492e41ce519553032a0fd6
Matías Estrada <matiasestrada.i@gmail.com>
2013-09-02 12:48:12 -0400

    Added scale set helper script

---
+++ classify/helpers/scale_set.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+""" This is a module that scale the set usin svm-scale
+"""
+
+__author__ = "Nicolas, Matias"
+__version__ = "0.1"
+
+import os
+import subprocess
+
+from utils import logger
+
+LIBSVM_RELATIVE = 'classify/libs/libsvm/'
+
+if 'ASTROFY_HOME' in os.environ:
+    TRAINING_SET_PATH = os.environ['ASTROFY_HOME'] + 'set/training.set'
+    SCALED_TRAINING_SET_PATH = os.environ['ASTROFY_HOME'] + 'set/training_scale.set'
+    LIBSVM_PATH = os.environ['ASTROFY_HOME'] + LIBSVM_RELATIVE
+else:
+    TRAINING_SET_PATH = 'set/training.set'
+    SCALED_TRAINING_SET_PATH = 'set/training_scale.set'
+    LIBSVM_PATH = LIBSVM_RELATIVE
+
+
+def scale(
+    input_path=TRAINING_SET_PATH,
+    output_path=SCALED_TRAINING_SET_PATH,
+    svm_scale='svm-scale'):
+
+    COMMAND = "{svm_scale_path} {input_path} > {output_path}"
+
+    logger.info("Scaling the training set")
+    logger.info("Input: {}".format(input_path))
+    logger.info("Output: {}".format(output_path))
+
+    command = COMMAND.format(
+        svm_scale_path=LIBSVM_PATH + svm_scale,
+        input_path=input_path,
+        output_path=output_path)
+
+    logger.info("Executing command:  " + command)
+
+    os.system(command)
+
+    logger.info("Scaling process finished.")
+
+
+if __name__ == '__main__':
+
+    scale()
